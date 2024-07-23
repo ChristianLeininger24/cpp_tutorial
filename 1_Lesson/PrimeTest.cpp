6ac065cae3f756f42227aa2af028070531bace2e
programmer <info2016frei@gmail.com>
2024-07-23 17:06:17 +0200

    add more test cases

--- 1_Lesson/PrimeTest.cpp
+++ 1_Lesson/PrimeTest.cpp
@@ -4,7 +4,16 @@
 #include "./Prime.cpp"
 #include <gtest/gtest.h>
 
-TEST(Prime, checkIfPrime) { ASSERT_EQ(checkIfPrime(1), false); }
+TEST(Prime, checkIfPrime) {
+  ASSERT_EQ(checkIfPrime(1), false);
+  ASSERT_EQ(checkIfPrime(2), true);
+  ASSERT_EQ(checkIfPrime(3), true);
+  ASSERT_EQ(checkIfPrime(4), false);
+  ASSERT_EQ(checkIfPrime(5), true);
+  ASSERT_EQ(checkIfPrime(7), true);
+  ASSERT_EQ(checkIfPrime(9), false);
+  ASSERT_EQ(checkIfPrime(42), false);
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
